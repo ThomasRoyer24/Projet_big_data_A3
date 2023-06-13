d467d638d966c970ffe68c871fb0df2afe799ba2
forgeur35 <tesniere.matheo35@gmail.com>
2023-06-13 11:20:00 +0200

    ajout de commentaire

--- series_chrono.R
+++ series_chrono.R
@@ -3,23 +3,29 @@ data <- transform(data, date = as.chron(date))
 
 #début du code
 
+#initialisation
 mois = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 series_chrono_mois <- c()
 
-for (i in 1:12){
+#récupération nbr accident par mois selon l'indice
+for (i in 1:12){ 
   series_chrono_mois <- c(series_chrono_mois, length(data$date[months(data$date)==mois[i]]))
 }
 
+#initialisation
 series_chrono_semaine <- c()
 ref_date <- chron("01/04/09")
 
+#on récupère la première valeur
 series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= dates(ref_date)]))
 ref_date <- ref_date+7
 
-for (i in 2:53){
+#récupération nbr accident par semaine selon l'indice
+for (i in 2:53){ 
   series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= dates(ref_date)]) - length(data$date[dates(data$date[]) <= dates(ref_date)-7]))
   ref_date <- ref_date+7
 }
 
+#Affichage graphique des données obtenu
 plot(x = 1:12, xlab = "mois", y = series_chrono_mois, ylab = "nbr accidents", type = "b", main = "Nombre accident par mois sur 2009")
 plot(x = 1:53, xlab = "semaines", y = series_chrono_semaine, ylab = "nbr accidents", type = "b", main = "Nombre accident par semaine sur 2009")
