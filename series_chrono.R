41c20f3d897a18fb1a787530c45446f68ad0703d
forgeur35 <tesniere.matheo35@gmail.com>
2023-06-13 11:03:13 +0200

    ajout de plot

--- series_chrono.R
+++ series_chrono.R
@@ -5,26 +5,21 @@ data <- transform(data, date = as.chron(date))
 
 mois = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 series_chrono_mois <- c()
-series_chrono_semaine <- c()
 
 for (i in 1:12){
   series_chrono_mois <- c(series_chrono_mois, length(data$date[months(data$date)==mois[i]]))
 }
 
-series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= "01/04/09"]))
-
+series_chrono_semaine <- c()
 ref_date <- chron("01/04/09")
 
-series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= "01/04/09"]))
+series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= dates(ref_date)]))
+ref_date <- ref_date+7
 
-for (i in 2:52){
-  
-  series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) > dates(ref_date)] & data$date[dates(data$date[]) <= dates(ref_date)+7]))
+for (i in 2:53){
+  series_chrono_semaine <- c(series_chrono_semaine, length(data$date[dates(data$date[]) <= dates(ref_date)]) - length(data$date[dates(data$date[]) <= dates(ref_date)-7]))
   ref_date <- ref_date+7
 }
 
-
-
-
-
-
+plot(x = 1:12, xlab = "mois", y = series_chrono_mois, ylab = "nbr accidents", type = "b", main = "Nombre accident par mois sur 2009")
+plot(x = 1:53, xlab = "semaines", y = series_chrono_semaine, ylab = "nbr accidents", type = "b", main = "Nombre accident par semaine sur 2009")
