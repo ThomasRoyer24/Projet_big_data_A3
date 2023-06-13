5f0d81c4f90b628368fe8d803985e92655ca7f1b
Thomas <thomas.royer24@gmail.com>
2023-06-13 14:30:34 +0200

    region fixed

--- ACP.R
+++ ACP.R
@@ -3,6 +3,10 @@ departement_data <- read.table("departement.txt", header = TRUE, fill = TRUE)
 region_data <- read_delim("regions.txt", delim = "\t", col_names = TRUE)
 demographie_data <- read_delim("Démographie.txt", delim = "\t", col_names = TRUE)
 
+Resultat <- data.frame()
+x <- c("region", "nb_acc_1", "nb_acc_2", "nb_acc_3", "nb_acc_4")
+Resultat <- data.frame(region=character(), nb_acc_1=numeric(), nb_acc_2=numeric(), nb_acc_3=numeric(), nb_acc_4=numeric())
+
 for(i in 1:nrow(region_data)){
   region = region_data[i,"REGION"]
   nb_acc_1 = 0
@@ -26,6 +30,6 @@ for(i in 1:nrow(region_data)){
       }
     }
   }
-  c(region_data[i,5],nb_acc_1,nb_acc_2,nb_acc_3,nb_acc_4)
+  Resultat[nrow(Resultat) + 1,]  <- c(region_data[i,5],nb_acc_1,nb_acc_2,nb_acc_3,nb_acc_4)
 }
 
