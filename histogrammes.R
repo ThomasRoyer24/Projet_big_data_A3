80cf59308af97f3e162bf69f481b017f5ef63585
forgeur35 <tesniere.matheo35@gmail.com>
2023-06-15 08:51:01 +0200

    arrangement titre histo

--- histogrammes.R
+++ histogrammes.R
@@ -1,9 +1,9 @@
 #Nombre accident selon tranches ages
-install.packages('ggplot2')
+#install.packages('ggplot2')
 library(ggplot2)
 
 series_age <- c()
-age <- c(">10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100", "100-110", "110-120", ">120")
+age <- c(">10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100", "100-110", "110-120", "<120")
 
 for (i in seq(0, 120, by=10)){
   series_age <- c(series_age, length(data$age[data$age>=i & data$age<i+10]))
@@ -12,7 +12,7 @@ for (i in seq(0, 120, by=10)){
 
 
 
-barplot(series_age, names.arg = age, main = "Nombre de mort par tranches d'âges")
+barplot(series_age, names.arg = age, main = "Nombre d'accident par tranches d'âges")
 
 #Moyenne mensuelle des accidents 
 #Rependre variables et données du fichier series_chrono.R
@@ -23,9 +23,9 @@ mois <- c(mois, "mean")
 barplot(xlab = "mois",
         series_chrono_mois,
         names.arg = mois,
-        ylab = "nbr accidents",
+        ylab = "nombre accidents",
         type = "b",
         main = "Nombre accident par mois sur 2009",
         col = c("gray","gray","gray","gray","gray","gray","gray","gray","gray","gray","gray","gray", "red"))
 
-        
+print(series_chrono_mois[13])
